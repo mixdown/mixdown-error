61c09a8762efe70c12d3035359cb374cab204ee0
Jason M Harrison <jason.m.harrison@gmail.com>
2014-07-16 13:46:13 -0500

    Update index.js
    Updated module pattern according to new pattern.

--- index.js
+++ index.js
@@ -1,47 +1,55 @@
+var BasePlugin = require('../../index.js').Plugin;
 var _ = require('lodash');
 
-module.exports = function(namespace) {
-  namespace = namespace || 'error';
+module.exports = BasePlugin.extend({
 
-  this.attach = function(options) {
+  init: function(namespace) {
+    namespace = namespace || 'error';
 
-    // handle the weirdness of errors in javascript
-    var formatError = function(err) {
-      var msg = '';
+    this.attach = function(options) {
 
-      if (typeof(err) === 'string') {
-        return err;
-      }
-      
-      if (err && err.message) {
-        msg += err.message;
-      }
+      // handle the weirdness of errors in javascript
+      var formatError = function(err) {
+        var msg = '';
 
-      if (err && err.stack) {
-        msg += '\n' + err.stack;
-      }
-
-      return msg;
-    };
-
-    this[namespace] = {
-
-      fail: function(err, res, headers) {
-        if (!res.headersSent) {
-          res.writeHead(500, _.extend({ 'Content-Type': 'text/plain'}, headers) );
+        if (typeof(err) === 'string') {
+          return err;
         }
-        
-        res.end('500 Error on page (if you got http 200, then the response failed mid-stream)\n' + formatError(err));
-      },
 
-      notFound: function(err, res, headers) {
-        if (!res.headersSent) {
-          res.writeHead(404, _.extend({ 'Content-Type': 'text/plain'}, headers) );
+        if (err && err.message) {
+          msg += err.message;
         }
-        res.end('404 Not Found (if you got http 200, then the response failed mid-stream)\n' + formatError(err));
-      }
+
+        if (err && err.stack) {
+          msg += '\n' + err.stack;
+        }
+
+        return msg;
+      };
+
+      this[namespace] = {
+
+        fail: function(err, res, headers) {
+          if (!res.headersSent) {
+            res.writeHead(500, _.extend({
+              'Content-Type': 'text/plain'
+            }, headers));
+          }
+
+          res.end('500 Error on page (if you got http 200, then the response failed mid-stream)\n' + formatError(err));
+        },
+
+        notFound: function(err, res, headers) {
+          if (!res.headersSent) {
+            res.writeHead(404, _.extend({
+              'Content-Type': 'text/plain'
+            }, headers));
+          }
+          res.end('404 Not Found (if you got http 200, then the response failed mid-stream)\n' + formatError(err));
+        }
+
+      };
 
     };
-
-  };
-};
+  }
+});
