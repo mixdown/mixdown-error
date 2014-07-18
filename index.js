3c35a1ad1f5fcd82497ba784f209dbd446785742
Tommy Messbauer <totally.bro@gmail.com>
2014-07-18 18:10:20 -0500

    added not authenticated and not authorized handlers

--- index.js
+++ index.js
@@ -24,6 +24,28 @@ module.exports = BasePlugin.extend({
     res.end('404 Not Found\n(if you get an http 200 then the request failed upstream)\nError: not found test\n    at Context.<anonymous>' + this.formatError(err));
   },
 
+  notAuthenticated: function(err, httpContext, headers) {
+    var res = httpContext.response;
+    if (!res.headersSent) {
+      res.writeHead(401, _.extend({
+        'Content-Type': 'text/plain'
+      }, headers));
+    }
+
+    res.end('401 - Not Logged In ' + this.formatError(err));
+  },
+
+  notAuthorized: function(err, httpContext, headers) {
+    var res = httpContext.response;
+    if (!res.headersSent) {
+      res.writeHead(403, _.extend({
+        'Content-Type': 'text/plain'
+      }, headers));
+    }
+
+    res.end('403 - Forbidden ' + this.formatError(err));
+  },
+
   formatError: function(err) {
 
     var msg = '';
